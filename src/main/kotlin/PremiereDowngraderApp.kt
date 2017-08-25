3e7dbbe90403ec4eea589cc4c8e649e6b9af1746
spookyuser <spookyUnknownUser@users.noreply.github.com>
2017-08-25 23:49:43 +0200

    Basic view

---
+++ src/main/kotlin/PremiereDowngraderApp.kt
@@ -0,0 +1,7 @@
+import tornadofx.*
+
+/**
+ * Created on 25/08/2017.
+ */
+
+class PremiereDowngraderApp : App(MainView::class)
---
+++ src/main/kotlin/StartApp.kt
@@ -0,0 +1,8 @@
+import javafx.application.Application
+
+/**
+ * Created on 25/08/2017.
+ */
+fun main(args: Array<String>) {
+    Application.launch(PremiereDowngraderApp::class.java, *args)
+}
